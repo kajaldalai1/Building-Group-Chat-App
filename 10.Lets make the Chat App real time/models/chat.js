a969169b87daaf56f98eb7e767ff27738179e44e
Kajal Dalai <kajaldalai1@gmail.com>
2024-02-16 02:26:31 +0530

    10.Lets make the Chat App real time

---
+++ 10.Lets make the Chat App real time/models/chat.js
@@ -0,0 +1,19 @@
+const Sequelize=require('sequelize')
+const sequelize=require('../util/chat')
+
+const expense=sequelize.define('chat',{
+    id:{
+        type:Sequelize.INTEGER,
+        autoIncrement:true,
+        allowNull:false,
+        primaryKey:true,
+        unique:true
+    },   
+    message:{
+        type:Sequelize.STRING,   
+        allowNull:false,
+    }  
+
+})
+
+module.exports=expense;
